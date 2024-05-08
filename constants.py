c4728dd231014525e925716648fb6fc905eb1b87
VishalH100 <vharsora007@gmail.com>
2024-05-08 02:16:49 +0100

    Changing from absolute to relative path

---
+++ constants.py
@@ -0,0 +1,19 @@
+# dataset filepath
+
+fb_data_18_19 = r'./datasets/original/Football Data 18-19.csv'
+fb_data_19_20 = r'./datasets/original/Football Data 19-20.csv'
+fb_data_20_21 = r'./datasets/original/Football Data 20-21.csv'
+fb_data_21_22 = r'./datasets/original/Football Data 21-22.csv'
+fb_data_22_23 = r'./datasets/original/Football Data 22-23.csv'
+
+combined_df_filepath = r'./datasets/preprocessed/Combined Football Data.csv'
+
+EPL_2018_2023 = r'./datasets/original/EPL 2018-2023.csv'
+
+final_data = r'./datasets/preprocessed/Final EPL 2018-2023.csv'
+
+GB_pred = r'./output/GB Predictions.csv'
+RF_pred = r'./output/RF Predictions.csv'
+RNN_pred = r'./output/RNN Predictions.csv'
+
+final_standings = r'./output/Final Standings.csv'
